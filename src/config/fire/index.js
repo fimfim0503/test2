a7b188f04cebab2263f3ce0c6d73f939a905eeb1
Tutu_gitlab <icammultazam@gmail.com>
2021-01-27 11:33:04 +0700

    setting firebase selesai

---
+++ src/config/fire/index.js
@@ -0,0 +1,13 @@
+import firebase from firebase;
+
+firebase.initializeApp({
+    apiKey: "AIzaSyC3Vp0j7JMF1RkYiYb7nag5PXn4vxjXIyQ",
+    authDomain: "my-doctor-f3577.firebaseapp.com",
+    projectId: "my-doctor-f3577",
+    storageBucket: "my-doctor-f3577.appspot.com",
+    messagingSenderId: "335434554240",
+    appId: "1:335434554240:web:97bc1535d5159c86edbf65"
+});
+
+const Fire = firebase;
+export default Fire;
